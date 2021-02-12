0934414f6878287dba1122397bbad29d303e259e
Talisha White <misstalishawhite@gmail.com>
2021-02-12 19:12:33 +0000

    the correct test file, base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,7 @@
+import uuid
+from datetime import datetime
+import models
+
 """
 Write a class BaseModel that defines all common attributes/methods for other classes:
 models/base_model.py
@@ -18,3 +22,23 @@ format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
 you can use isoformat() of datetime object
 This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel
 """
+#!/usr/bin/python3
+class BaseModel:
+  def __init__(self):
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = self.created_at
+  
+  def __str__(self):
+    return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+  def save(self):
+    self.updated_at = datetime.now()
+
+  def to_dict(self):
+    _dict = self.__dict__.copy()
+    _dict["__class__"] = self.__class__.__name__
+    return _dict
+
+
+
